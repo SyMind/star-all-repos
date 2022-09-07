8b00b28d693def7aca58c74d9cb33a800a24afed
SyMind <dacongsama@live.com>
2022-09-07 20:55:24 +0800

    use code from issue title

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
+import fetch from 'node-fetch'
 import { Octokit } from 'octokit'
 
+// https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps#2-users-are-redirected-back-to-your-site-by-github
+const response = await fetch('https://github.com/login/oauth/access_token', {
+    method: 'post',
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        client_id: 'e9547e631cc7b7bb1d6f',
+        client_secret: process.env.CLIENT_SECRET,
+        code: process.env.AUTH_CODE
+    })
+})
+const {access_token} = await response.json()
+
 const octokit = new Octokit({
-    auth: process.env.GITHUB_TOKEN
+    auth: access_token
 })
 
 const repos = await octokit.request('GET /users/{username}/repos', {
