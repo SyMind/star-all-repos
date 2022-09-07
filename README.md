f223a560b2fa5aa11638d1563c1655ca5f254bad
SyMind <dacongsama@live.com>
2022-09-07 21:32:11 +0800

    traverse all repos

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Star all repos 🤩
 
-[Click here](https://github.com/login/oauth/authorize?client_id=e9547e631cc7b7bb1d6f&redirect_uri=https://symind.github.io/star-all-repos&scope=public_repo)
+点两下，Star 我的所有项目！
+
+1. 点击[这里](https://github.com/login/oauth/authorize?client_id=e9547e631cc7b7bb1d6f&redirect_uri=https://symind.github.io/star-all-repos&scope=public_repo)使用 GitHub OAuth 进行授权。
+
+2. 点击提交 issue 按钮。
--- index.js
+++ index.js
@@ -28,11 +28,11 @@ if (repos.status !== 200) {
     process.exit(-1)
 }
 
-const repo = repos.data[0]
-
-octokit.request('PUT /user/starred/{owner}/{repo}', {
-    owner: repo.owner.login,
-    repo: repo.name
-}).then(() => {
-    console.log('⭐️ ' + repo.name)
-})
+for (const repo of repos) {
+    octokit.request('PUT /user/starred/{owner}/{repo}', {
+        owner: repo.owner.login,
+        repo: repo.name
+    }).then(() => {
+        console.log('⭐️ ' + repo.name)
+    })
+}
