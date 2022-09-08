c78021e66161bb120544dafa7445678365e01af5
SyMind <dacongsama@live.com>
2022-09-08 08:08:16 +0800

    create a comment to thank you

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 import fetch from 'node-fetch'
 import { Octokit } from 'octokit'
 
+console.log('Repository owner: ', process.env.REPO_OWNER)
+
+const ownerOctokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN
+})
+
+const comment = `<p align="center"><b>感谢你来 Star 我的项目！❤️ </b></p>
+<p align="center"> 
+  <kbd>
+    <img src="https://github.com/SyMind/star-all-repos/blob/main/images/ikun.gif?raw=true">
+  </kbd>
+</p>`
+
+ownerOctokit.request('POST /repos/{owner}/{repo}/issues/{issue_number}/comments', {
+    owner: process.env.REPO_OWNER,
+    repo: 'star-all-repos',
+    issue_number: Number(process.env.ISSUE_NUMBER),
+    body: comment
+})
+
 // https://docs.github.com/en/developers/apps/building-oauth-apps/authorizing-oauth-apps#2-users-are-redirected-back-to-your-site-by-github
 const response = await fetch('https://github.com/login/oauth/access_token', {
     method: 'post',
@@ -16,13 +36,11 @@ const response = await fetch('https://github.com/login/oauth/access_token', {
 })
 const {access_token} = await response.json()
 
-const octokit = new Octokit({
+const userOctokit = new Octokit({
     auth: access_token
 })
 
-console.log('Repository owner: ', process.env.REPO_OWNER)
-
-const repos = await octokit.request('GET /users/{username}/repos', {
+const repos = await userOctokit.request('GET /users/{username}/repos', {
     username: process.env.REPO_OWNER,
     per_page: 100
 })
@@ -32,7 +50,7 @@ if (repos.status !== 200) {
 }
 
 for (const repo of repos.data) {
-    octokit.request('PUT /user/starred/{owner}/{repo}', {
+    userOctokit.request('PUT /user/starred/{owner}/{repo}', {
         owner: repo.owner.login,
         repo: repo.name
     }).then(() => {
